6719b833d182b6d91be08ba97774462343a85e86
Zombie-dev-1 <84056391+Zombie-dev-1@users.noreply.github.com>
2024-03-01 19:05:19 +0530

    Update and rename omni_veux.mk to twrp_veux.mk
    Changes for Twrp

--- twrp_veux.mk
+++ twrp_veux.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
 PRODUCT_DEVICE := veux
-PRODUCT_NAME := omni_veux
+PRODUCT_NAME := twrp_veux
 PRODUCT_BRAND := qti
 PRODUCT_MODEL := Holi for arm64
 PRODUCT_MANUFACTURER := xiaomi
