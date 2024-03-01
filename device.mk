83cd1fe1880f2eacfa4eff0f996e5e180e132d8e
Zombie-dev-1 <84056391+Zombie-dev-1@users.noreply.github.com>
2024-03-01 21:01:10 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -23,12 +23,6 @@ PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
-
-PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libpiex_shim
 
