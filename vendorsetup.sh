cad42a70cc21ceb4be8846e864772f4234dac6b1
Arpitshubhamm <157899697+Arpitshubhamm@users.noreply.github.com>
2024-03-01 21:32:02 +0530

    Updated omni stuff

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_veux-user
-add_lunch_combo omni_veux-userdebug
-add_lunch_combo omni_veux-eng
+add_lunch_combo twrp_veux-user
+add_lunch_combo twrp_veux-userdebug
+add_lunch_combo twrp_veux-eng
