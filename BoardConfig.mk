444d5941867fc7327453ec1a7fda8db48d73fb30
Zombie-dev-1 <84056391+Zombie-dev-1@users.noreply.github.com>
2024-03-03 07:37:41 +0530

    Update prebuilt kernel path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/veux
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
 ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/veux/kernel
 endif
 
 # Partitions
